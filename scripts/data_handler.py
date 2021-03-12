1fbec6375ada6c6d0b7c37f38bb059e22b64b36a
Jonathan Smyth <jonathan.smyth@hhi.fraunhofer.de>
2021-03-12 22:19:47 +0100

    cleaning the nans

--- scripts/data_handler.py
+++ scripts/data_handler.py
@@ -1,14 +1,12 @@
 import pandas as pd
 
 def get_total_data():
-    df = pd.read_csv('../data/Telematik_Events.csv', sep=';')
+    df = pd.read_csv('./Telematik_Events.csv', sep=';')
     # print(df)
+    data = df[['lat','lon','dur']].dropna()
 
-    event_type = df['type']
-    lat = df['lat']
-    lon = df['lon']
-    dur = df['dur']
+    return data.values
 
-    return df[['lat','lon','dur']].values
+# t = get_total_data()
+# print(t)
 
-#get_total_data()
