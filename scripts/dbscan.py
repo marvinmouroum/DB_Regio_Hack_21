691bb3ff4e1b578096e856f38fd55981bff84706
Marvin Mouroum <mouroum.m@gmail.com>
2021-03-13 00:53:55 +0100

    getting there

--- scripts/dbscan.py
+++ scripts/dbscan.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 X, mean, std = data.get_total_data('DECEL_020G_15KMH_30KMH')
 
-db = DBSCAN(eps=0.4, min_samples=5)
+db = DBSCAN(eps=0.8, min_samples=5)
 db.fit(X)
 
 y_pred = db.fit_predict(X)
@@ -24,6 +24,8 @@ plt.figure(figsize=(10,6))
 plt.scatter(x[:,0], x[:,1], cmap='Paired')
 plt.title("Dangerous locations")
 plt.show()
+
+print(x)
 
 ax = data.plot_3D(X,y_pred)
 
--- scripts/kmeans.py
+++ scripts/kmeans.py
@@ -30,10 +30,10 @@ a3 = 2
 
 #find_optimal_number_of_clusters()
 
-kmeans = KMeans(n_clusters=11, init='k-means++', max_iter=300, n_init=10, random_state=0)
+kmeans = KMeans(n_clusters=20, init='k-means++', max_iter=300, n_init=10, random_state=0)
 pred_y = kmeans.fit_predict(X)
-#plt.scatter(X[:,a1], X[:,a2], X[:,a3])
+plt.scatter(X[:,a1], X[:,a2], X[:,a3])
 #plt.scatter(kmeans.cluster_centers_[:, a1], kmeans.cluster_centers_[:, a2], kmeans.cluster_centers_[:, a3], s=300, c='red')
-#plt.show()
+plt.show()
 
 data.plot_3D(X,pred_y)
